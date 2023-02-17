5891ab06b4db82cc272e4503c6c8c24d2e6bae7b
Ian Kamau <ian.kamau@smartapplicationsgroup.com>
2023-02-17 11:02:00 +0300

    Deployment Script changed

--- UserDB/Script.PostDeployment1.sql
+++ UserDB/Script.PostDeployment1.sql
@@ -12,8 +12,8 @@ Post-Deployment Script Template
 if not exists (select 1 from dbo.[User])
 begin
     insert into dbo.[User] (FirstName, LastName)
-    values ('Tim', 'Corey'),
-    ('Sue', 'Storm'),
+    values ('Jon', 'Doe'),
+    ('Susan', 'Storm'),
     ('John', 'Doe'),
-    ('Mary', 'Poppins');
+    ('Jane', 'Doe');
 end
